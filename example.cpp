57b54bee8b9d223e5f12c3b52a015f40d2214300
ZacksterUG <48206034+ZacksterUG@users.noreply.github.com>
2021-01-02 22:41:47 +0500

    Add files via upload

---
+++ example.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include <string.h>
+#include <math.h>
+#include "ZackString.h"
+using namespace std;
+//using namespace ZacksterString;
+int main()
+{
+	ZacksterString::str a, b, c;
+	ZacksterString::str k("Hello world!");
+	a = "Hello";
+	b = " world";
+	a += b;
+	c = a + "!!!";
+	c[5] = 'R';
+	cout << c.line() << endl;
+	cout << k.line() << endl;
+	return 0;
+}
